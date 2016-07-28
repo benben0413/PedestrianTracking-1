34960c599d912e262077ad5a05c3f4dbf5555d6a
Fabián <souto_94@hotmail.com>
2016-07-28 18:54:40 -0400

    Fixed some bugs with the pedestrians colors.

--- detect.py
+++ detect.py
@@ -33,13 +33,14 @@ colors = [(0, 255, 0),
 		  (255, 0, 0),
 		  (0, 0, 255),
 		  (0, 0, 0),
-		  (100, 100, 100)]
+		  (100, 100, 100),
+		  (255, 0, 213),
+		  (0, 213, 255),
+		  (0, 114, 42)]
 # True if it is available that color, false otherwise
-available = [True,
-			 True,
-			 True,
-			 True,
-			 True]
+available = []
+for i in range(len(colors)):
+	available.append(True)
 
 # Returns the first available color.
 def getColor():
@@ -53,6 +54,11 @@ def setAvailable(color):
 	for (i, cl) in enumerate(colors):
 		if (cl == color):
 			available[i] = True
+
+def setNotAvailable(color):
+	for (i, cl) in enumerate(colors):
+		if (color==cl):
+			available[i] = False
 
 # # Next color available.
 # nextColor = 0
@@ -84,9 +90,7 @@ class Pedestrian:
 		self.y = y
 		self.color = color
 		# Set available false
-		for (i, cl) in enumerate(colors):
-			if (color==cl):
-				available[i] = False
+		setNotAvailable(color)
 		self.frame = initNumFrame
 	# Returns the color of the pedestrian:
 	def getColor(self):
@@ -110,12 +114,13 @@ pedestrians = []
 # Check if 2 pedestrians are the same probably by their positions:
 def checkSamePed(ped1, ped2):
 	# threshold in pixels. If theirs middle points are 20 pixels close or less.
-	threshold = 10
+	thresholdX = 20
+	thresholdY = 50
 	(x1, y1) = ped1.getPosition()
 	(x2, y2) = ped2.getPosition()
 	dx = np.abs(x2-x1)
 	dy = np.abs(y2-y1)
-	if (dx < 20) and (dy < 20):
+	if (dx < thresholdX) and (dy < thresholdY):
 		return True
 	else:
 		return False
@@ -151,7 +156,7 @@ def addPedestrian(ped, currentNumFrame):
 def removeOldPed(currentNumFrame):
 	global pedestrians
 	# threshold for delete a pedestrian from the list: measured in frames.
-	threshold = 20 #If the pedestrian does not appear in 20 frames is deleted from the list.
+	threshold = 10 #If the pedestrian does not appear in threshold frames is deleted from the list.
 	for ped in pedestrians:
 		lastNumFrameSawed = ped.getNumFrame()
 		df = np.abs(lastNumFrameSawed-currentNumFrame)
@@ -162,7 +167,7 @@ def removeOldPed(currentNumFrame):
 
 # getCentralPos returns the central point of a rectangle:
 def getCentralPos(xA,yA,xB,yB):
-	return (np.abs(xA-xB), np.abs(yA-yB))
+	return ((xA+xB)/2, (yA+yB)/2)
 
 
 # Define the codec and create VideoWriter object
@@ -171,49 +176,60 @@ out = cv2.VideoWriter('output.avi',fourcc, 60.0, (640,480))
 
 
 # Loop over the frames over the video:
+aux = 0
 while True:
 
 	# grab the current frame
 	(grabbed, frame) = camera.read()
-	# load the image and resize it to (1) reduce detection time
-	# and (2) improve detection accuracy
-	image = imutils.resize(frame, width=min(640, frame.shape[1]))
 
-	# detect people in the image
-	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
-		padding=(8, 8), scale=1.05)
+	aux = aux + 1
+	if (aux%5) == 0:
 
-	# apply non-maxima suppression to the bounding boxes using a
-	# fairly large overlap threshold to try to maintain overlapping
-	# boxes that are still people
-	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+		# load the image and resize it to (1) reduce detection time
+		# and (2) improve detection accuracy
+		image = imutils.resize(frame, width=min(640, frame.shape[1]))
 
-	# draw the final bounding boxes
-	for (xA, yA, xB, yB) in pick:
-		# Create a pedestrian:
-		(x,y) = getCentralPos(xA,yA,xB,yB)
-		color = getColor()
-		ped = Pedestrian(x, y, color, count)
-		# Add the pedestrian or check if he is in the list:
-		ped2 = addPedestrian(ped, count)
-		# Remove pedestrians that does not appear more
-		removeOldPed(count)
-		rectangleColor = ped2.getColor()
-		
-		cv2.rectangle(image, (xA, yA), (xB, yB), rectangleColor, 2)
+		# detect people in the image
+		(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+			padding=(8, 8), scale=1.05)
 
-	# write the flipped frame
-	if grabbed==True:
-		writeimage = imutils.resize(image, width=640, height=480)
-		out.write(writeimage)
+		# apply non-maxima suppression to the bounding boxes using a
+		# fairly large overlap threshold to try to maintain overlapping
+		# boxes that are still people
+		rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+		pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-	# show the output frame
-	cv2.imshow("Frame", image)
-	count += 1
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-		break
+		# draw the final bounding boxes
+		for (xA, yA, xB, yB) in pick:
+			# Create a pedestrian:
+			(x,y) = getCentralPos(xA,yA,xB,yB)
+			color = getColor()
+			ped = Pedestrian(x, y, color, count)
+			# Add the pedestrian or check if he is in the list:
+			ped2 = addPedestrian(ped, count)
+			# Remove pedestrians that does not appear more
+			removeOldPed(count)
+			rectangleColor = ped2.getColor()
+
+			print(ped.getColor(), ped.getPosition())
+			print(ped2.getColor(), ped.getPosition())
+			print("**")
+
+			cv2.rectangle(image, (xA, yA), (xB, yB), rectangleColor, 2)
+
+		print("------")
+
+		# write the flipped frame
+		if grabbed==True:
+			writeimage = imutils.resize(image, width=640, height=480)
+			out.write(writeimage)
+
+		# show the output frame
+		cv2.imshow("Frame", image)
+		count += 1
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("q"):
+			break
 
 # Release everything if job is finished
 camera.release()
